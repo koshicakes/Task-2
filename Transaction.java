4357b76ac4332a5943f90c35f191151968d78ddc
koshicakes <chardi.valentos@gmail.com>
2026-02-09 00:04:17 +0800

    Edited Transaction File

--- Transaction.java
+++ Transaction.java
@@ -1,17 +1,15 @@
 public class Transaction {
     public Customer customer;
-    public Product productItem;
+    public Product product;
 
     // Constructor
-    public Transaction() {
-        customer = new Customer();
-        customer.fullName = "Default Customer";
-
-        productItem = new Product();
-        productItem.modelName = "Default Product";
-        prod
-        uctItem.price = 0.0;
+    public Transaction(Customer c) {
+        customer = c;
+        product= new Product();
+        product.modelName = "Default";
+        product.price = 0.0;
     }
+
     //Identifier
     public String identify() {
         return "I am the Transaction class. I record customer purchases.";
@@ -21,6 +19,6 @@ public class Transaction {
     public String getReport() {
         return "----- TRANSACTION REPORT -----\n"
                + customer.getDetails() + "\n"
-               + productItem.getDetails();
+               + product.getDetails();
     }
 }
